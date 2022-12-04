48ea1150028d3c1c3a1036f896888e2bcb8e7b95
Семен Киреков <sakireko@mts.ru>
2022-12-04 22:18:53 +0300

    enhance User entity

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,27 +1,55 @@
 package com.example.domain;
 
+import com.example.domain.converter.PhoneNumberConverter;
+import com.example.domain.value.PhoneNumber;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import java.io.Serializable;
 import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Table(name = "users")
 @Getter
 public class User {
-  @Id
-  private UUID id;
+    @EmbeddedId
+    private ID id;
 
-  @Column(name = "first_name")
-  private String firstName;
+    @Column(name = "phone_number")
+    @Convert(converter = PhoneNumberConverter.class)
+    @NotNull
+    private PhoneNumber phoneNumber;
 
-  @Column(name = "last_name")
-  private String lastName;
+    public static User newUser(PhoneNumber phoneNumber) {
+        final var user = new User();
+        user.phoneNumber = phoneNumber;
+        user.id = new User.ID(UUID.randomUUID());
+        return user;
+    }
 
-  private String snils;
-
-  @Column(name = "phone_number")
-  private long phoneNumber;
+    @Data
+    @Setter(PRIVATE)
+    @Embeddable
+    @AllArgsConstructor
+    @NoArgsConstructor(access = PROTECTED)
+    public static class ID implements Serializable {
+        @Column(updatable = false)
+        @NotNull
+        private UUID id;
+    }
 }
